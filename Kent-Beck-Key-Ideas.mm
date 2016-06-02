186f093208d9df6937a4e3925e4dfdaf73049cee
Romilly Cocking <romilly.cocking@gmail.com>
2016-06-02 18:07:12 +0100

    More images

--- Kent-Beck-Key-Ideas.mm
+++ Kent-Beck-Key-Ideas.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.3.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1464878216703">
+<node ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1464884691583" LINK="https://www.facebook.com/notes/kent-beck/a-searching-and-fearless-intellectual-inventory/1179765038723025">
 <icon BUILTIN="password"/>
 <richcontent TYPE="NODE">
 
@@ -18,8 +18,8 @@
   </body>
 </html>
 </richcontent>
-<hook NAME="MapStyle">
-    <properties show_note_icons="true"/>
+<hook NAME="MapStyle" zoom="0.5185846">
+    <properties show_note_icons="true" show_icon_for_attributes="true"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node">
@@ -68,13 +68,50 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="11"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
 <hook URI="images/kent.jpg" SIZE="1.0" NAME="ExternalObject"/>
-<node TEXT="Behaviour" POSITION="right" ID="ID_433737641" CREATED="1464862567184" MODIFIED="1464862571973">
+<richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Ideas taken with permission from Kent Beck's Facebook Post https://www.facebook.com/notes/kent-beck/a-searching-and-fearless-intellectual-inventory/1179765038723025
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="Model" POSITION="right" ID="ID_656163859" CREATED="1464862426020" MODIFIED="1464862481474">
+<edge COLOR="#00ffff"/>
+<node TEXT="Pattern" ID="ID_439940815" CREATED="1464862499745" MODIFIED="1464884935415">
+<hook URI="images/Composite_UML_class_diagram_(fixed).svg.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="Paradigm" ID="ID_865722309" CREATED="1464880307713" MODIFIED="1464880310918"/>
+<node TEXT="Fractal" ID="ID_437663994" CREATED="1464862518665" MODIFIED="1464885071798">
+<hook URI="images/Julia_set_(ice).png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Behaviour" POSITION="right" ID="ID_433737641" CREATED="1464862567184" MODIFIED="1464886857508">
 <edge COLOR="#ffff00"/>
-<node TEXT="Scarcity" ID="ID_699289882" CREATED="1464862579943" MODIFIED="1464862583348"/>
+<node TEXT="Scarcity" ID="ID_699289882" CREATED="1464862579943" MODIFIED="1464886857504" HGAP="-67" VSHIFT="-71"/>
 <node TEXT="Sufficiency" ID="ID_685404738" CREATED="1464862583887" MODIFIED="1464862605140"/>
 <node TEXT="Hero&apos;s Journey" ID="ID_1129069387" CREATED="1464862774107" MODIFIED="1464862781472"/>
+<node TEXT="Negotiation" ID="ID_627687210" CREATED="1464879557628" MODIFIED="1464879562585"/>
+<node TEXT="Succession" ID="ID_1199727169" CREATED="1464879586364" MODIFIED="1464879758010">
+<hook URI="images/salami.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+<node TEXT="Indirect Approach" ID="ID_520667638" CREATED="1464879806371" MODIFIED="1464879814171"/>
+<node TEXT="Accountability" ID="ID_1929057193" CREATED="1464879986554" MODIFIED="1464879991686"/>
+<node TEXT="Manipulation" ID="ID_1321997373" CREATED="1464880006785" MODIFIED="1464880016317"/>
+<node TEXT="Authenticity" ID="ID_437346799" CREATED="1464880027761" MODIFIED="1464880031198"/>
+<node TEXT="Bids" ID="ID_799919636" CREATED="1464880041200" MODIFIED="1464880043476"/>
+<node TEXT="Detachment" ID="ID_131332595" CREATED="1464880057199" MODIFIED="1464880062660"/>
+<node TEXT="Operant Conditioning" ID="ID_1105846044" CREATED="1464880103918" MODIFIED="1464880110971"/>
+<node TEXT="Maslow&apos;s Hierarchy" ID="ID_609969604" CREATED="1464880112950" MODIFIED="1464880119251"/>
+<node TEXT="Waiting" ID="ID_21573885" CREATED="1464880564883" MODIFIED="1464880569183"/>
 </node>
 <node TEXT="Systems Thinking" POSITION="right" ID="ID_1383149466" CREATED="1464862147474" MODIFIED="1464874306113" LINK="http://www.amazon.com/dp/1603580557/">
 <edge COLOR="#ff0000"/>
@@ -102,18 +139,35 @@
 </node>
 <node TEXT="Reversibility" ID="ID_1656443241" CREATED="1464862899920" MODIFIED="1464862905005"/>
 <node TEXT="Quality Software Management: Systems Thinking" ID="ID_1282002995" CREATED="1464874555297" MODIFIED="1464874571104" LINK="http://www.amazon.com/Quality-Software-Management-Systems-Thinking/dp"/>
+<node TEXT="Emergence" ID="ID_1120389245" CREATED="1464880255851" MODIFIED="1464880259679"/>
+<node TEXT="Butterfly effect" ID="ID_1277857411" CREATED="1464880477886" MODIFIED="1464886583311"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=495592
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook URI="images/Lorenz_attractor_yb.svg.png" SIZE="1.0" NAME="ExternalObject"/>
 </node>
-<node TEXT="Model" POSITION="left" ID="ID_656163859" CREATED="1464862426020" MODIFIED="1464862481474">
-<edge COLOR="#00ffff"/>
-<node TEXT="Pattern" ID="ID_439940815" CREATED="1464862499745" MODIFIED="1464862503526"/>
-<node TEXT="Fractal" ID="ID_437663994" CREATED="1464862518665" MODIFIED="1464862521726"/>
+<node TEXT="Boundaries" ID="ID_247410942" CREATED="1464880515988" MODIFIED="1464880519984"/>
 </node>
 <node TEXT="Software" POSITION="left" ID="ID_1778192391" CREATED="1464862218655" MODIFIED="1464862222229">
 <edge COLOR="#0000ff"/>
-<node TEXT="Recursion" ID="ID_918709353" CREATED="1464862364596" MODIFIED="1464862368753"/>
+<node TEXT="Recursion" ID="ID_918709353" CREATED="1464862364596" MODIFIED="1464886859644">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="80" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_437663994" STARTINCLINATION="-91;-122;" ENDINCLINATION="-151;19;" STARTARROW="DEFAULT" ENDARROW="DEFAULT"/>
+</node>
 <node TEXT="Program" ID="ID_904194333" CREATED="1464862223463" MODIFIED="1464862225885">
 <node TEXT="Subroutine" ID="ID_15121906" CREATED="1464862344005" MODIFIED="1464862347857"/>
-<node TEXT="Object" ID="ID_1983892428" CREATED="1464862444114" MODIFIED="1464862447079"/>
+<node TEXT="Object" ID="ID_1983892428" CREATED="1464862444114" MODIFIED="1464885593041">
+<hook URI="images/vwin.jpg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
 </node>
 <node TEXT="IPO" ID="ID_1686204324" CREATED="1464862381892" MODIFIED="1464878131334"><richcontent TYPE="DETAILS">
 
@@ -140,7 +194,6 @@
     </p>
   </body>
 </html>
-
 </richcontent>
 <hook URI="images/IPO.png" SIZE="1.0" NAME="ExternalObject"/>
 </node>
@@ -152,8 +205,26 @@
 <edge COLOR="#7c0000"/>
 <node TEXT="Test" ID="ID_1481054982" CREATED="1464862394708" MODIFIED="1464862697885"/>
 <node TEXT="Hypothesis" ID="ID_1828093811" CREATED="1464862699965" MODIFIED="1464862704858"/>
-<node TEXT="Bayes" ID="ID_1408803322" CREATED="1464862706405" MODIFIED="1464862709778"/>
+<node TEXT="Bayes" ID="ID_1408803322" CREATED="1464862706405" MODIFIED="1464885833925"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      By mattbuck (category) - Own work by mattbuck., CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=14658489
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook URI="images/320px-Bayes&apos;_Theorem_MMB_01.jpg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
 <node TEXT="Uncertainty" ID="ID_1306661667" CREATED="1464862710493" MODIFIED="1464862713665"/>
+<node TEXT="Metrics" ID="ID_1354784682" CREATED="1464879434144" MODIFIED="1464879436788"/>
+<node TEXT="Invalidation" ID="ID_3811204" CREATED="1464880394671" MODIFIED="1464880398956"/>
+<node TEXT="Implication" ID="ID_1194001277" CREATED="1464880432654" MODIFIED="1464880442906"/>
 </node>
 <node TEXT="Financial" POSITION="right" ID="ID_1129012907" CREATED="1464862736204" MODIFIED="1464862746801">
 <edge COLOR="#00007c"/>
@@ -173,17 +244,13 @@
 </richcontent>
 </node>
 <node TEXT="Concave/Convex" ID="ID_926453832" CREATED="1464862917704" MODIFIED="1464862924708"/>
-</node>
-<node TEXT="Statistics" POSITION="right" ID="ID_1183096759" CREATED="1464862804363" MODIFIED="1464862808111">
-<edge COLOR="#007c00"/>
-<node TEXT="Distributions" ID="ID_18955201" CREATED="1464862809266" MODIFIED="1464862813751">
-<node TEXT="Gaussian" ID="ID_1586145951" CREATED="1464862832130" MODIFIED="1464862838095"/>
-<node TEXT="Power-law" ID="ID_1353071499" CREATED="1464862838642" MODIFIED="1464862843135"/>
-</node>
+<node TEXT="Diminshing Returns" ID="ID_1607518048" CREATED="1464880583090" MODIFIED="1464880588543"/>
 </node>
 <node TEXT="Manufacturing" POSITION="left" ID="ID_1365929608" CREATED="1464862853690" MODIFIED="1464862857247">
 <edge COLOR="#7c007c"/>
-<node TEXT="Assembly Line" ID="ID_1695798848" CREATED="1464862858201" MODIFIED="1464862861319"/>
+<node TEXT="Assembly Line" ID="ID_1695798848" CREATED="1464862858201" MODIFIED="1464885937608">
+<hook URI="images/Ford_assembly_line_-_1913.jpg" SIZE="1.0" NAME="ExternalObject"/>
+</node>
 <node TEXT="JIT" ID="ID_1916896794" CREATED="1464862861945" MODIFIED="1464863670752"><richcontent TYPE="DETAILS">
 
 <html>
@@ -199,6 +266,51 @@
 </richcontent>
 </node>
 <node TEXT="Buffer" ID="ID_65450030" CREATED="1464862876794" MODIFIED="1464862879230"/>
+</node>
+<node TEXT="Innovation" POSITION="left" ID="ID_758248165" CREATED="1464880621076" MODIFIED="1464880678540">
+<edge COLOR="#ff0000"/>
+<node TEXT="The Chasm" ID="ID_339861581" CREATED="1464880680553" MODIFIED="1464886446933"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      CC BY 2.5, https://en.wikipedia.org/w/index.php?curid=11484459
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<hook URI="images/220px-DiffusionOfInnovation.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Design" POSITION="left" ID="ID_112189867" CREATED="1464879508686" MODIFIED="1464879511722">
+<edge COLOR="#7c7c00"/>
+<node TEXT="Tradeoffs" ID="ID_1920490323" CREATED="1464879526773" MODIFIED="1464879530138"/>
+<node TEXT="Transcendance" ID="ID_794409304" CREATED="1464879541678" MODIFIED="1464879546394"/>
+<node TEXT="Affordance/Mapping" ID="ID_1636261936" CREATED="1464880279066" MODIFIED="1464880285142"/>
+</node>
+<node TEXT="Thinking" POSITION="left" ID="ID_1680302805" CREATED="1464880743432" MODIFIED="1464880753476">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Metaphor" ID="ID_164924885" CREATED="1464880754989" MODIFIED="1464880758482"/>
+<node TEXT="Moral Hazard" ID="ID_1877215528" CREATED="1464880772989" MODIFIED="1464880783634"/>
+</node>
+<node TEXT="Mathematics" POSITION="left" ID="ID_1183096759" CREATED="1464862804363" MODIFIED="1464879386417">
+<edge COLOR="#007c00"/>
+<node TEXT="Derivatives" ID="ID_1400739225" CREATED="1464879389689" MODIFIED="1464879394542"/>
+<node TEXT="Statistics" ID="ID_1860770169" CREATED="1464879396937" MODIFIED="1464879401101">
+<node TEXT="Distributions" ID="ID_18955201" CREATED="1464862809266" MODIFIED="1464862813751">
+<node TEXT="Gaussian" ID="ID_1586145951" CREATED="1464862832130" MODIFIED="1464862838095"/>
+<node TEXT="Power-law" ID="ID_1353071499" CREATED="1464862838642" MODIFIED="1464862843135"/>
+</node>
+<node TEXT="Variance" ID="ID_1855165911" CREATED="1464880458134" MODIFIED="1464880461050"/>
+</node>
+<node TEXT="Optimization" ID="ID_1423513850" CREATED="1464879446431" MODIFIED="1464879450556"/>
+<node TEXT="Local Maxima" ID="ID_384127734" CREATED="1464879456783" MODIFIED="1464879462220"/>
+<node TEXT="Pareto" ID="ID_1832010192" CREATED="1464879475070" MODIFIED="1464879478740"/>
+<node TEXT="Rewriting" ID="ID_1628434312" CREATED="1464880355856" MODIFIED="1464880359189"/>
 </node>
 </node>
 </map>
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 A mind map of Kent Beck's [Facebook post](https://www.facebook.com/notes/kent-beck/a-searching-and-fearless-intellectual-inventory/1179765038723025)
  about key ideas that shape his work
 
-![Mapping Kent's FB Post](images/Kent-Beck-Key-Ideas.jpg)
+![Mapping Kent's FB Post](images/fp-map.png)
